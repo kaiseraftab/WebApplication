6fdb2b76886e0eeeff4fa8cf3294b7a2ebd3e5f0
Kaiseraftab <kaiser.aftab@student.nhlstenden.com>
2022-03-04 18:16:36 +0100

    updated the header and footer

--- controlRobot.php
+++ controlRobot.php
@@ -2,66 +2,32 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title></title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" type="text/css" href="./style/style.css">
+  <link rel="icon" href="./assets/PBBwhite.png">
+  <title>Battle Bot Events</title>
+  <?php
+    echo "
+      <style>
+        .header-style .nav .menu .events{
+          color: #83c0ff;
+        }
+        .header-style .nav .menu .events:hover{
+          color: #0386FF;
+          text-decoration: none;
+        }
+      </style>";
+  ?>
 </head>
-<div class="header-style">
-  <div class="nav">
-    <input type="checkbox">
-    <span></span>
-    <div class="menu">
-      <li><a href="index.php" class="home">Home</a></li>
-      <li class="line"> &VerticalLine;</li>
-      <li><a href="events.php" class="events">Events</a></li>
-      <li class="line"> &VerticalLine;</li>
-      <li><a href="leaderboard.php" class="lead">Leaderboard</a></li>
-      <li class="line"> &VerticalLine;</li>
-      <li><a href="#" class="bets">Bets</a></li>
-      <li class="line"> &VerticalLine;</li>
-      <li><a href="#" class="login">Login</a></li>
-      <li><a href="#" class="control_robot">Control Robot</a></li>
-      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    </div>
-    <div class="h-sign">
-      <h2>&larr; Header</h2>
-    </div>
-  </div>
-</div>
+<body>
+<div class="body">
+  <?php
+    include_once "header.html";
+  ?>
+
+<?php
+  include_once "footer.html";
+?>
+</body>
 </html>
-
-<div class="footer-top">
-
-  <a href="index.php">
-    <img src="assets/PBBblack.png" class="PBBlogo" alt="NHL Stenden - Project Battle Bots"
-
-         title="NHL Stenden - Project Battle Bots">
-  </a>
-
-  <div class="icons">
-    <ul class="social-icons">
-      <li><a class="facebook" href="https://www.facebook.com/nhlstenden/"><i class="fa fa-facebook" style="padding-top:13px;"></i></a></li>
-      <li><a class="whatsapp" href="https://api.whatsapp.com/send?phone=31613579931"><i class="fa fa-whatsapp" style="padding-top:13px;"></i></a>
-      </li>
-      <li><a class="instagram" href="https://www.instagram.com/nhlstenden/?hl=en"><i class="fa fa-instagram" style="padding-top:13px;"></i></a>
-      </li>
-      <li><a class="reddit" href="https://www.reddit.com/r/Stenden/"><i class="fa fa-reddit" style="padding-top:13px;"></i></a></li>
-    </ul>
-  </div>
-
-  <div class="text-address">
-    <a href="https://goo.gl/maps/Yu2T2cTjgKYpGXLU6" style="text-decoration: none;">
-      <div class="uni-name"><b>NHL Stenden - Emmen</b></div>
-      <div>Van Schaikweg 94,</div>
-      <div>7811 KL Emmen</div>
-      <div>The Netherlands</div>
-    </a>
-  </div>
-
-  <div class="text-contact">
-    <div style="font-weight: bold;">Contact us!</div>
-    <div><a href="mailto:pbbsupport@nhlstenden.com">pbbsupport@nhlstenden.com</a></div>
-    <div><a href="mailto:info@nhlstenden.com">info@nhlstenden.com</a></div>
-    <div>Tel: <a href="tel:+31 58 244 1155">+31 58 244 1155</a></div>
-  </div>
-
-</div>
